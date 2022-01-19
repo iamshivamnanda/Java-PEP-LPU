87db44fedbf5753c7e9b0f7d44363cf160de0e90
shivamnanda436 <38239120+shivamnanda436@users.noreply.github.com>
2022-01-19 08:36:52 +0530

    Getting Started

---
+++ Java-LPU/src/day1/MyApp.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class MyApp {
+public static void main(String[] args) {
+	System.out.println("[MyApp] Our Goal is to develop solutions to save time :)");
+    System.out.println("[MyApp] Be Exceptional");
+	}
+}
---
+++ session-2/src/DataTypes.java
@@ -0,0 +1,9 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		System.out.println("DATA Types");
+
+	}
+
+}
